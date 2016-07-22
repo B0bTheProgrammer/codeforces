c07916d9b423cdfb2b630bc93fac84bb8e6d79bd
B0bTheProgrammer <treeaeter@gmail.com>
2016-07-22 23:55:43 +0400

    a simplest solution to a problem

--- contest691/C691A/main.cpp
+++ contest691/C691A/main.cpp
@@ -1,8 +1,42 @@
-#include <QCoreApplication>
-
-int main(int argc, char *argv[])
+#include <iostream>
+#include <istream>
+#include <stdlib.h>
+#include <string>
+using namespace std;
+int main()
 {
-    QCoreApplication a(argc, argv);
+int n;
+char s;
+cin >> s;
+n = atoi(&s);
+char a[n];
+for(int i=0;i < n;i++)
+{
+ cin >> a[i];
+}
+/*cout << endl;
+for(int i=0;i < n;i++)
+{
+ cout << atoi(&a[i]);
+}*/
+if(n == 1)
+{
+ if (atoi(a) == 0)
+ {
+  cout << "NO";
+ }else{
+  cout << "YES";
+ }
+}else{
+    int k = 0;
+    for(int i=0;i < n;i++)
+    {
+        if(a[i] == '0')k++;
+    }
+    if(k == 1){cout << "YES";
+    }else{cout << "NO";}
+}
 
-    return a.exec();
+
+    return 0;
 }
